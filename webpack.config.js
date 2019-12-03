dcb64f5f1921910b4b94edc5ee101731fe07f888
Alexander Shuvalov <ashuvduddits@gmail.com>
2019-12-03 23:06:14 +0300

    vp_1 started

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,71 @@
 let HtmlPlugin = require('html-webpack-plugin');
 let { CleanWebpackPlugin } = require('clean-webpack-plugin');
-let MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+let MiniCssExtractPlugin = require('mini-css-extract-plugin');
 let UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-let rules = require('./webpack.config.rules');
 let path = require('path');
 
-rules.push({
-    test: /\.css$/,
-    use: [
-        MiniCSSExtractPlugin.loader,
-        {
-            loader: 'style-loader' 
-        },
-        {
-            loader: 'css-loader' 
-        }
-    ]
-});
-
 module.exports = {
-    entry: {
-        main: './src/cookie.js'
-    },
-    devServer: {
-        index: 'index.html'
-    },
-    output: {
-        filename: '[name].[hash].js',
-        path: path.resolve('dist')
-    },
-    devtool: 'source-map',
-    module: { rules },
-    plugins: [
-        new UglifyJsPlugin({
-            sourceMap: true,
-            uglifyOptions: {
-                compress: {
-                    drop_debugger: false
-                }
-            }
-        }),
-        new MiniCSSExtractPlugin('styles.css'),
-        new HtmlPlugin({
-            title: 'Main Homework',
-            template: 'src/cookie.hbs',
-            filename: 'index.html',
-            chunks: ['main']
-        }),
-        new CleanWebpackPlugin()
-    ]
+	entry: {
+		main: './src/index.js'
+	},
+	devServer: {
+		index: 'index.html'
+	},
+	output: {
+		filename: '[name].[hash].js',
+		path: path.resolve('dist')
+	},
+	devtool: 'source-map',
+	module: { 
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					{
+						loader: MiniCssExtractPlugin.loader
+					},
+					'css-loader',
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.hbs/,
+				loader: 'handlebars-loader'
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg|eot|ttf|woff|woff2)$/i,
+				loader: 'file-loader',
+				options: {
+					name: '[hash:8].[ext]',
+					outputPath: 'resources'
+				}
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new UglifyJsPlugin({
+			sourceMap: true,
+			uglifyOptions: {
+				compress: {
+					drop_debugger: false
+				}
+			}
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		}),
+		new HtmlPlugin({
+			title: 'API Yandex Maps',
+			template: 'src/index.hbs',
+			filename: 'index.html',
+			chunks: ['main']
+		})
+	]
 };
