1f1b92a4b6f2b92792c3db0b2b652fb137af68b5
Alexander Shuvalov <ashuvduddits@gmail.com>
2019-12-07 23:57:18 +0300

    add modal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 require('./main.scss');
 ymaps.ready(init);
+
+let geoObjects = [];
+
 function init() {
   // Создание карты.
-  let myMap = new ymaps.Map("map", {
+  let map = new ymaps.Map("map", {
     // Координаты центра карты.
     // Порядок по умолчанию: «широта, долгота».
     // Чтобы не определять координаты центра карты вручную,
@@ -11,6 +14,33 @@ function init() {
     // Уровень масштабирования. Допустимые значения:
     // от 0 (весь мир) до 19.
     zoom: 12,
-    // controls: ['zoomControl']
+    // controls: ['zoomControl'],
+    behaviors: ['drag']
   });
+  let placemark = geoObjects.push(new ymaps.Placemark([55.32, 42.18], {
+      hintContent: "Это хинт",
+      balloonContent: "Это балун"
+    },
+    // {
+    //   iconLayout: "default#image",
+    //   iconImageHref: "",
+    //   iconImageSize: [46, 57],
+    //   iconImageOffset: [-23, -57]
+    // }
+  ))
+  // map.geoObjects.add(placemark);
+
+  let clusterer = new ymaps.Clusterer({
+    clusterIcons: [
+      {
+        href: "",
+        size: [100, 100],
+        offset: [-50, -50]
+      }
+    ],
+    clusterIconContentLayout: null
+  })
+
+  map.geoObjects.add(clusterer);
+  clusterer.add(geoObjects)
 }
